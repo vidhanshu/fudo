a924cec8391c03ad71dadc6dfc02206cb7b4557b
vidhanshu <vidhanshu.borade.cs@ghrce.raisoni.net>
2023-06-25 15:07:31 +0530

    Add menu images

--- src/home/utils/constants.ts
+++ src/home/utils/constants.ts
@@ -3,42 +3,36 @@ export const MENU = [
     _id: 1,
     name: "Spicy Samosa",
     price: 10,
-    image:
-      "https://static.toiimg.com/thumb/61050397.cms?imgsize=246859&width=800&height=800",
+    image: "/home/samosa.webp",
   },
   {
     _id: 2,
-    name: "Spicy Samosa",
+    name: "Spicy Burger",
     price: 10,
-    image:
-      "https://static.toiimg.com/thumb/61050397.cms?imgsize=246859&width=800&height=800",
+    image: "/home/burger.jpg",
   },
   {
     _id: 3,
-    name: "Spicy Samosa",
+    name: "Litchi Shake",
     price: 10,
-    image:
-      "https://static.toiimg.com/thumb/61050397.cms?imgsize=246859&width=800&height=800",
+    image: "/home/litchi.jpg",
   },
   {
     _id: 4,
     name: "Spicy Samosa",
     price: 10,
-    image:
-      "https://static.toiimg.com/thumb/61050397.cms?imgsize=246859&width=800&height=800",
+    image: "/home/samosa.webp",
   },
   {
     _id: 5,
-    name: "Spicy Samosa",
+    name: "Spicy Burger",
     price: 10,
-    image:
-      "https://static.toiimg.com/thumb/61050397.cms?imgsize=246859&width=800&height=800",
+    image: "/home/burger.jpg",
   },
   {
     _id: 6,
-    name: "Spicy Samosa",
+    name: "Litchi Shake",
     price: 10,
-    image:
-      "https://static.toiimg.com/thumb/61050397.cms?imgsize=246859&width=800&height=800",
+    image: "/home/litchi.jpg",
   },
 ];
